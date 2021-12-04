61964555da0b2ecb9397ebc52c4c32b1a351a5cc
gmrgameusko <94599240+gmrgameusko@users.noreply.github.com>
2021-12-04 19:58:22 +0300

    Update index.js

--- countdown-generator/index.js
+++ countdown-generator/index.js
@@ -102,7 +102,7 @@ module.exports = {
         });
         
         // estimate the font size based on the provided width
-        let fontSize = Math.floor(this.width / 25) + 'px';
+        let fontSize = Math.floor(this.width / 23) + 'px';
         let fontFamily = 'Courier New'; // monospace works slightly better
         
         // set the font style
